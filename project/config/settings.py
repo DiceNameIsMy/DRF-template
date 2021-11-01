3dc528b2d868c60be772006ad5be01b923b1705b
DiceNameIsMy <dicenameismy@gmail.com>
2021-11-01 10:23:35 +0600

    fix: add forgotten corsheaders middleware

--- project/config/settings.py
+++ project/config/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
